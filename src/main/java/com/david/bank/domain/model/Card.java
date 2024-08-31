01bc0db7a1842223becd75e5a9189fd619be4d0f
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2024-08-31 09:58:12 -0300

    create classes

---
+++ src/main/java/com/david/bank/domain/model/Card.java
@@ -0,0 +1,34 @@
+package com.david.bank.domain.model;
+
+import java.math.BigDecimal;
+
+public class Card {
+
+    private long id;
+    private String number;
+    private BigDecimal limit;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
---
+++ src/main/java/com/david/bank/domain/model/Feature.java
@@ -0,0 +1,32 @@
+package com.david.bank.domain.model;
+
+public class Feature {
+
+    private long id;
+    private String icon;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
